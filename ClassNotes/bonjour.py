d94d22d8ae3313bbc605875951da99f6f0c56b59
Doc1000 <doster1000@hotmail.com>
2017-10-23 20:15:07 -0600

    working in class

---
+++ ClassNotes/bonjour.py
@@ -0,0 +1,30 @@
+
+# you can launch jupyter notebook with ath term from the bash
+
+i = 0
+fin = 10
+acc  = 0
+
+while i < fin:
+    acc += i
+    i += 1
+print(i)
+print (acc)
+
+print(str(i) + ' digits accumulate to ' + str(acc))
+
+
+i = 0ppy
+fin = 10
+acc  = 0
+
+while True:
+    acc += i
+    i += 1
+    if i >= fin:
+        break
+print(str(acc) + ' of second')
+
+#you can use continue as well - conditional under while loop - ignores everything after it and goes back up to top of while loop
+
+#pass is equivalent of telling python to do nothing
---
+++ ClassNotes/hello.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+x = input('x: ')
+y = input('y: ')
+
+if x > y:
+    print('x gt y')
+elif x==y:
+    print('x et y')
+else:
+    print('x is lt y')
+
+# hash is read as a commment
+
+# if not (True and (True or False)):  # if statements automatically resolve to indentation
+#     print('If statement is True')
+#     #print('This is also controlled by IF')
+# elif True and True:
+#     print('elif is True')
+# else: print('If Statement is False')
+# wassap
+#you use cntl-/ to comment out a section of code
+#print('Outside the if!')
