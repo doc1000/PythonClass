f3bbef48e9b60fe28510c3fc114ae4de77ce0c8a
Doc1000 <doster1000@hotmail.com>
2017-10-25 13:51:05 -0600

    changed members attribute

--- homework/week4class7/hard.py
+++ homework/week4class7/hard.py
@@ -1,6 +1,5 @@
 class OurClass():
-
-    def __init__(self, name, location, size=0):
+    def __init__(self, name, location, size=0, members = None):
         self.name = name
         self.location = location
         self.size = size
@@ -9,16 +8,23 @@ class OurClass():
             self.at_capacity = True
         else:
             self.at_capacity = False
+        if members == None:
+            self.members = []
+        else:
+            self.members = members
+        pass
+
+    def check_if_at_capacity(self):
+       return self.at_capacity
 
    # def add_question_asked(self, question):
    #      self.questions_asked.append(question)
 
-   def add_class_members(self, num):
+    def add_class_members(self, num):
         self.size += num
 
         if self.size >= 20:
             print('Capacity Reached!!')
             self.at_capacity = True
 
-   def check_if_at_capacity(self):
-        return self.at_capacity
+# myC = OurClass('english','galv',14)
